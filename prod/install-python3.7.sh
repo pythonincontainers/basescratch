8d8ce7682a9261987056911b1494d837726adb75
pythonincontainers <45329364+pythonincontainers@users.noreply.github.com>
2019-05-30 08:17:50 +0200

    Update install-python3.7.sh

--- prod/install-python3.7.sh
+++ prod/install-python3.7.sh
@@ -1,13 +1,13 @@
 apt-get update
 # Install 'software-properties-common' to get add-apt-repository
-apt-get install -y software-properties-common
+apt-get install -y --no-install-recommends software-properties-common
 # Install latest Python repository for Ubuntu
 add-apt-repository ppa:deadsnakes/ppa -y
 apt-get update
 # Purge and remove 'software-properties-common', it is large a not necessary in Production Image
 apt-get --purge autoremove -y software-properties-common
-# Install Python 3.7 and its Libraries plus 'curl' to pull PIP installation script
-apt-get install -y python3.7 python3.7-dev curl
+# Install Python 3.7 plus 'curl' to pull PIP installation script
+apt-get install -y --no-install-recommends python3.7 curl
 # Add 'python' name to 'python3.7' binary
 ln /usr/bin/python3.7 /usr/bin/python
 # Pull PIP installation script
@@ -18,7 +18,8 @@ rm /tmp/get-pip.py
 # Purge and remove Ubuntu Packages not needed in Python Production Image, 'curl' for example
 apt-get remove --auto-remove --purge -y --allow-remove-essential $(cat /tmp/packages_to_purge.txt)
 # Clean APT caches
-apt-get clean
+apt-get clean && rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
 # Create a full system Tar ball
 tar -cpzf rootfs.tar.gz --exclude=/rootfs.tar.gz --one-file-system /
+
